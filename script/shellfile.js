663cd2b3510a229df583a3a96c91298f66d3f850
meitung473 <a8870506@gmail.com>
2022-06-25 22:23:43 +0800

    update:Readme

--- script/shellfile.js
+++ script/shellfile.js
@@ -3,7 +3,11 @@ const FOLDERNAME = "LIOJ";
 
 function testfile(filename) {
     let path = FOLDERNAME + "/" + filename + "/";
-    shell.cat(path + filename + ".txt").exec(`node ${path + filename}.js`);
+    shell
+        .cat(path + filename + ".txt")
+        .exec(`node ${path + filename}.js`, { async: true }, (err) => {
+            if (err) throw err;
+        });
 }
 
 module.exports = testfile;
